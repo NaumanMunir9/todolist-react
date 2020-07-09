13a6d9db1faf329977c79a16ae2e248d128f8a5b
Nauman Munir <naumanmunir@gmail.com>
2020-07-09 13:07:43 +0500

    setState in Appjs

--- src/App.js
+++ src/App.js
@@ -6,27 +6,34 @@ import TodoInput from "./components/TodoInput";
 
 export class App extends Component {
   state = {
-    items: [
-      {
-        id: 1,
-        title: "wake up",
-      },
-      {
-        id: 2,
-        title: "make breakfast",
-      },
-    ],
+    items: [],
     id: uuidv4(),
     item: "",
     editItem: false,
   };
 
   handleChange = (e) => {
-    console.log("handle change");
+    this.setState({
+      item: e.target.value,
+    });
   };
 
   handleSubmit = (e) => {
-    console.log("handle change");
+    e.preventDefault();
+    const newItem = {
+      id: this.state.id,
+      title: this.state.item,
+    };
+    const updatedItems = [...this.state.items, newItem];
+    this.setState(
+      {
+        items: updatedItems,
+        item: "",
+        id: uuidv4(),
+        editItem: false,
+      },
+      () => console.log(this.state)
+    );
   };
 
   clearList = () => {
