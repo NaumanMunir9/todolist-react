6806d8bc8f58c66fcdf8fd4afc89798204000526
Nauman Munir <naumanmunir@gmail.com>
2020-07-09 15:20:48 +0500

    todo Item commit

--- src/App.js
+++ src/App.js
@@ -25,15 +25,12 @@ export class App extends Component {
       title: this.state.item,
     };
     const updatedItems = [...this.state.items, newItem];
-    this.setState(
-      {
-        items: updatedItems,
-        item: "",
-        id: uuidv4(),
-        editItem: false,
-      },
-      () => console.log(this.state)
-    );
+    this.setState({
+      items: updatedItems,
+      item: "",
+      id: uuidv4(),
+      editItem: false,
+    });
   };
 
   clearList = () => {
