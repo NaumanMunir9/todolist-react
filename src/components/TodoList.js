c0f2c262dbd9f9da19ecaee38b6992d4f9f58fc1
Nauman Munir <naumanmunir@gmail.com>
2020-07-09 15:09:48 +0500

    commit Todo List

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,13 +3,28 @@ import TodoItem from "./TodoItem";
 
 export class TodoList extends Component {
   render() {
+    const { items, clearList, handleDelete, handleEdit } = this.props;
     return (
-      <section>
-        <h2>
-          Todo List
-        </h2>
-        <TodoItem />
-      </section>
+      <ul className="list-group my-5">
+        <h3 className="text-capitalize text-center">todo List</h3>
+        {items.map((item) => {
+          return (
+            <TodoItem
+              key={item.id}
+              title={item.title}
+              handleDelete={() => handleDelete(item.id)}
+              handleEdit={() => handleEdit(item.id)}
+            />
+          );
+        })}
+        <button
+          type="button"
+          className="btn btn-danger btn-block text-uppercase mt-5"
+          onClick={clearList}
+        >
+          Clear List
+        </button>
+      </ul>
     );
   }
 }
